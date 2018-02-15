6d6176ce3229c8ce91c2dadbc5f257897f9d21de
anders hopland <anders@dhcp-10-22-79-207.wlan.ntnu.no>
2018-02-15 15:23:54 +0100

    Added 4k video

--- main.py
+++ main.py
@@ -7,20 +7,22 @@ cap = cv2.VideoCapture('udp://@192.168.1.1:9999')
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out1 = cv2.VideoWriter('output1.avi', fourcc, 30.0, (500, 500))
 out2 = cv2.VideoWriter('output2.avi', fourcc, 30.0, (500, 500))
-#out3 = cv2.VideoWriter('output3.avi', fourcc, 30.0, (500, 500))
-#out4 = cv2.VideoWriter('output4.avi', fourcc, 30.0, (500, 500))
-#out5 = cv2.VideoWriter('output5.avi', fourcc, 30.0, (500, 500))
+out3 = cv2.VideoWriter('output3.avi', fourcc, 30.0, (500, 500))
+out4 = cv2.VideoWriter('output4.avi', fourcc, 30.0, (500, 500))
+out5 = cv2.VideoWriter('output5.avi', fourcc, 30.0, (500, 500))
 
 while(cap.isOpened):
     ret, frame = cap.read()
 
     ret, frame = cap.read()
     if ret:
-        frame1 = frame[100:600, 100:600]
+        frame1 = frame[100:600, 600:1100]
         frame2 = frame[100:600, 600:1100]
-        frame3 = frame[600:1100, 100:600]
-        frame4 = frame[700:1200, 300:800]
-        frame5 = frame[600:1100, 0:500]
+        frame3 = frame[100:600, 600:1100]
+        frame4 = frame[100:600, 600:1100]
+        frame5 = frame[100:600, 600:1100]
+
+        cv2.imshow('frame', frame1)
         # write the flipped frame
         out1.write(frame1)
         out2.write(frame2)
@@ -36,7 +38,7 @@ cap.release()
 
 out1.release()
 out2.release()
-#out3.release()
-#out4.release()
-#out5.release()
+out3.release()
+out4.release()
+out5.release()
 cv2.destroyAllWindows()
